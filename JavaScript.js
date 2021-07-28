2108491ad1dd732908b32a1491d7b324c6ccc4a9
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2021-07-27 22:15:05 -0300

    tabuada

--- JavaScript.js
+++ JavaScript.js
@@ -33,114 +33,114 @@ document.write(`Esta foi sua <u>palavra</u>: ${palavra} <br>`)
 addEventListener('click', clicando)
 
 function clicando() {
-    document.getElementById("elementodiv").style.backgroundColor="red";
-    document.getElementById("elementodiv").innerText="Clicou";
-} 
+    document.getElementById("elementodiv").style.backgroundColor = "red";
+    document.getElementById("elementodiv").innerText = "Clicou";
+}
 
 function emcima() {
-    document.getElementById("elementodiv").style.backgroundColor="yellow";
-    document.getElementById("elementodiv").innerText="Em cima";
+    document.getElementById("elementodiv").style.backgroundColor = "yellow";
+    document.getElementById("elementodiv").innerText = "Em cima";
 }
 
 function fora() {
-    document.getElementById("elementodiv").style.backgroundColor="blue";
-    document.getElementById("elementodiv").innerText="Saiu";
+    document.getElementById("elementodiv").style.backgroundColor = "blue";
+    document.getElementById("elementodiv").innerText = "Saiu";
 }
 
 
-function somar(){
+function somar() {
     let num1 = document.getElementById("num1");
-    let num2 = document.getElementById("num2"); 
+    let num2 = document.getElementById("num2");
     let num5 = Number(num1.value)
     let num6 = Number(num2.value)
     let num3 = num5 + num6;
-    document.getElementById("num3").innerHTML= (`A soma entre ${num5} e o numero ${num6} resulta em: <strong>${num3}</strong>`);
+    document.getElementById("num3").innerHTML = (`A soma entre ${num5} e o numero ${num6} resulta em: <strong>${num3}</strong>`);
 }
 
-function velocid(){
+function velocid() {
     let valo = document.getElementById("vel")
     let valo1 = Number(valo.value)
-    document.getElementById("velocidade").innerHTML=(`A sua velocidade é de <strong>${valo1}</strong> Km/H`)
+    document.getElementById("velocidade").innerHTML = (`A sua velocidade é de <strong>${valo1}</strong> Km/H`)
     if (valo1 > 80) {
-        document.getElementById("ifresult").innerHTML=(`Você será <strong>multado!</strong>`)
+        document.getElementById("ifresult").innerHTML = (`Você será <strong>multado!</strong>`)
     }
     else {
-        document.getElementById("ifresult").innerHTML=(`Você um é <b>bom</b> motorista!`)
+        document.getElementById("ifresult").innerHTML = (`Você um é <b>bom</b> motorista!`)
     }
 }
 
 
-function possoDirigir () {
+function possoDirigir() {
     let idademotorista = document.querySelector('input.idadecarteira')
     let idademotoristafinal = Number(idademotorista.value)
 
-    if (idademotoristafinal >= 18 && idademotoristafinal< 60) {
-        document.querySelector('p.resultadocarteira').innerHTML=(`Sua idade é ${idademotoristafinal}, você esta autorizado a dirigir!`)
+    if (idademotoristafinal >= 18 && idademotoristafinal < 60) {
+        document.querySelector('p.resultadocarteira').innerHTML = (`Sua idade é ${idademotoristafinal}, você esta autorizado a dirigir!`)
     }
     else if (idademotoristafinal < 18) {
-        document.querySelector('p.resultadocarteira').innerHTML=(`Sua idade é ${idademotoristafinal}, você ainda é muito jovem para dirigir!`)
-    }  
+        document.querySelector('p.resultadocarteira').innerHTML = (`Sua idade é ${idademotoristafinal}, você ainda é muito jovem para dirigir!`)
+    }
     else {
-        document.querySelector("p.resultadocarteira").innerHTML=(`Sua idade é ${idademotoristafinal}, você pode dirigir mas precisa comprovar que encherga bem!`) 
+        document.querySelector("p.resultadocarteira").innerHTML = (`Sua idade é ${idademotoristafinal}, você pode dirigir mas precisa comprovar que encherga bem!`)
     }
 }
 
 
-function diasemana () {
+function diasemana() {
     /* Date objects are created with new Date().*/
     let now = new Date()
     /*The below getDay is a Date Object Methods*/
     let diadasemana = now.getDay()
     switch (diadasemana) {
         case 1:
-            document.querySelector('p.diasemana').innerHTML=(`Hoje é Segunda`)
+            document.querySelector('p.diasemana').innerHTML = (`Hoje é Segunda`)
             break
         case 2:
-            document.querySelector('p.diasemana').innerHTML=(`Hoje é Terça`)
+            document.querySelector('p.diasemana').innerHTML = (`Hoje é Terça`)
             break
         case 3:
-            document.querySelector('p.diasemana').innerHTML=(`Hoje é Quarta`)
+            document.querySelector('p.diasemana').innerHTML = (`Hoje é Quarta`)
             break
         case 4:
-            document.querySelector('p.diasemana').innerHTML=(`Hoje é Quinta`)
+            document.querySelector('p.diasemana').innerHTML = (`Hoje é Quinta`)
         case 5:
-            document.querySelector('p.diasemana').innerHTML=(`Hoje é Sexta`)
+            document.querySelector('p.diasemana').innerHTML = (`Hoje é Sexta`)
             break
         case 6:
-            document.querySelector('p.diasemana').innerHTML=(`Hoje é Sabado`)
+            document.querySelector('p.diasemana').innerHTML = (`Hoje é Sabado`)
         case 0:
-            document.querySelector('p.diasemana').innerHTML=(`Hoje é Domingo`)
+            document.querySelector('p.diasemana').innerHTML = (`Hoje é Domingo`)
             break
         default:
-            document.querySelector('p.diasemana').innerHTML=(`Não sei que dia é!`)
+            document.querySelector('p.diasemana').innerHTML = (`Não sei que dia é!`)
             break
-    }   
+    }
 }
 
 
 
 /*Exercicio dia*/
 
-function horadia(){
+function horadia() {
     let hora = new Date()
     let horaexata = hora.getHours()
 
-    document.getElementById("hora").innerHTML=(`<h3>Agora são ${horaexata} horas</h3>`)
-    if (horaexata > 19 || horaexata < 8  ) {
-        document.querySelector('div.imagem').style.backgroundImage="url('/images/noite.jpg')"
-        document.body.style.backgroundColor="rgb(6, 7, 94)"
+    document.getElementById("hora").innerHTML = (`<h3>Agora são ${horaexata} horas</h3>`)
+    if (horaexata > 19 || horaexata < 8) {
+        document.querySelector('div.imagem').style.backgroundImage = "url('/images/noite.jpg')"
+        document.body.style.backgroundColor = "rgb(6, 7, 94)"
     }
-    else if (horaexata > 8 && horaexata < 12  ) {
-        document.querySelector('div.imagem').style.backgroundImage="url('/images/manha.jpg')"
-        document.body.style.backgroundColor="rgb(13, 235, 13)"
+    else if (horaexata > 8 && horaexata < 12) {
+        document.querySelector('div.imagem').style.backgroundImage = "url('/images/manha.jpg')"
+        document.body.style.backgroundColor = "rgb(13, 235, 13)"
     }
-    else if (horaexata > 12 && horaexata < 16  ) {
-        document.querySelector('div.imagem').style.backgroundImage="url('/images/tarde.jpeg')"
-        document.body.style.backgroundColor="rgb(235, 220, 13)"
+    else if (horaexata > 12 && horaexata < 16) {
+        document.querySelector('div.imagem').style.backgroundImage = "url('/images/tarde.jpeg')"
+        document.body.style.backgroundColor = "rgb(235, 220, 13)"
     }
     else /*(horaexata > 16 && horaexata < 19  )*/ {
-        document.querySelector('div.imagem').style.backgroundImage="url('/images/tardinha.jpg')"
-        document.body.style.backgroundColor="rgb(248, 102, 4)"
+        document.querySelector('div.imagem').style.backgroundImage = "url('/images/tardinha.jpg')"
+        document.body.style.backgroundColor = "rgb(248, 102, 4)"
     }
 }
 
@@ -152,7 +152,7 @@ window.onload = horadia()
 
 /*Verificador idade*/
 
-function veridade(){
+function veridade() {
     /*Para o query selector tem que ser a classe não o ID do elemento*/
     let idadefinal1 = document.querySelector('input.smalltext')
     let idadeinter = Number(idadefinal1.value)
@@ -164,34 +164,34 @@ function veridade(){
     } else {
         var prefer = '"Sem sexo"'
     }
-    document.getElementById('fraseidade').innerHTML=(`Detectamos <strong>${prefer}</strong> com <strong>${idadefinal}</strong> anos de idade!`)
+    document.getElementById('fraseidade').innerHTML = (`Detectamos <strong>${prefer}</strong> com <strong>${idadefinal}</strong> anos de idade!`)
 
 
     /*Verificador homem*/
     if (idadefinal <= 5 && document.getElementById('masc').checked) {
-        document.getElementById('imagemage').style.backgroundImage="url('/images/menino.jpg')"
-    } 
+        document.getElementById('imagemage').style.backgroundImage = "url('/images/menino.jpg')"
+    }
     else if (idadefinal <= 15 && idadefinal > 5 && document.getElementById('masc').checked) {
-        document.getElementById('imagemage').style.backgroundImage="url('/images/guri.jpg')"
-    } 
+        document.getElementById('imagemage').style.backgroundImage = "url('/images/guri.jpg')"
+    }
     else if (idadefinal <= 50 && idadefinal > 15 && document.getElementById('masc').checked) {
-        document.getElementById('imagemage').style.backgroundImage="url('/images/homem.jpg')"
-    } 
+        document.getElementById('imagemage').style.backgroundImage = "url('/images/homem.jpg')"
+    }
     else if (idadefinal <= 150 && idadefinal > 50 && document.getElementById('masc').checked) {
-        document.getElementById('imagemage').style.backgroundImage="url('/images/idoso.jpg')"
+        document.getElementById('imagemage').style.backgroundImage = "url('/images/idoso.jpg')"
     }
     /*Verificador mulher*/
     if (idadefinal <= 5 && document.getElementById('femin').checked) {
-        document.getElementById('imagemage').style.backgroundImage="url('/images/menina.jpg')"
-    } 
+        document.getElementById('imagemage').style.backgroundImage = "url('/images/menina.jpg')"
+    }
     else if (idadefinal <= 15 && idadefinal > 5 && document.getElementById('femin').checked) {
-        document.getElementById('imagemage').style.backgroundImage="url('/images/guria.jpg')"
-    } 
+        document.getElementById('imagemage').style.backgroundImage = "url('/images/guria.jpg')"
+    }
     else if (idadefinal <= 50 && idadefinal > 15 && document.getElementById('femin').checked) {
-        document.getElementById('imagemage').style.backgroundImage="url('/images/mulher.jpg')"
-    } 
+        document.getElementById('imagemage').style.backgroundImage = "url('/images/mulher.jpg')"
+    }
     else if (idadefinal <= 150 && idadefinal > 50 && document.getElementById('femin').checked) {
-        document.getElementById('imagemage').style.backgroundImage="url('/images/idosa.jpg')"
+        document.getElementById('imagemage').style.backgroundImage = "url('/images/idosa.jpg')"
     }
 }
 
@@ -202,16 +202,17 @@ function veridade(){
 
 function countSpace() {
     let count1 = document.getElementById('countnumber1')
-    let countnumber1 = Number(count1.value) 
+    let countnumber1 = Number(count1.value)
 
     let count2 = document.getElementById('countnumber2')
-    let countnumber2 = Number(count2.value) 
+    let countnumber2 = Number(count2.value)
 
     let count3 = document.getElementById('countnumber3')
     let countnumber3 = Number(count3.value)
 
+
     /*Estrutura abaixo esta criando o elemento span no HTML*/
-    for (let countnumber1 = Number(count1.value);countnumber1 < countnumber2;countnumber1 += countnumber3) {
+    for (let countnumber1 = Number(count1.value); countnumber1 < countnumber2; countnumber1 += countnumber3) {
         /*usada span tag para numeros ficarem em linha*/
         let elementocriado = document.createElement("span")
         /*concatenado um espaço para os numeros não ficarem colados*/
@@ -219,10 +220,15 @@ function countSpace() {
         let conteudo = document.createTextNode(countnumber1 + ' - \u{1F929} ')
         elementocriado.appendChild(conteudo)
         let elementodestino = document.querySelector('div.countdiv')
-        elementodestino.appendChild(elementocriado) 
-   }
+        elementodestino.appendChild(elementocriado)
+    }
 }
 
 
 
+
+
+
 /*fazer ainda a contagem regressiva*/
+
+
